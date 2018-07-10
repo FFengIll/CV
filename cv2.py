4f987466c47caeb95d34da464106a48dc15a52b1
FFengIll <fengyouzheng@gmail.com>
2018-07-10 21:04:04 +0800

    add code for version 2.

--- cv2.py
+++ cv2.py
@@ -1,3 +1,4 @@
+
 import pystache
 import json
 import argparse
@@ -13,8 +14,7 @@ def render(template, data):
     parsed = pystache.parse(template)
     res = render.render(parsed, data)
     res = res.encode("utf-8")
-    with open("cv.html", 'wb') as fd:
-        fd.write(res)
+    return res
 
 
 class obj(object):
@@ -26,7 +26,8 @@ def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--setting", help="", action="store", default="setting.json", type=str, dest="setting")
     parser.add_argument("-d", "--data", help="", action="store", default="cv.json", type=str, dest="data")
-    parser.add_argument("-t", "--template", help="", action="store", default="templates/default_html.mustache", type=str, dest="template")
+    parser.add_argument("-t", "--template", help="", action="store", default="template_html.html", type=str, dest="template")
+    parser.add_argument("-l", "--layout", help="", action="store", default="template_html.html", type=str, dest="layout")
     parser.add_argument(nargs=argparse.REMAINDER, dest="value")
     args = parser.parse_args()
     return args
@@ -53,19 +54,27 @@ def main():
 
     with open(args.setting) as fd:
         format = json.load(fd)
+        
 
     data = combine_json(data, format)
+    s = json.dumps(data, indent=4, sort_keys=True)
+    print("setting: \n{}".format(data['settings']))
 
     with open(args.template) as fd:
         template = fd.readlines()
         template = ''.join(template)
         template = template.decode("unicode_escape")
+        res = render(template, data)
+        with open("cv.html", 'wb') as fd:
+            fd.write(res)
 
-    s = json.dumps(data, indent=4, sort_keys=True)
-    print("setting: \n{}".format(data['settings']))
-
-    render(template, data)
-
+    # with open(args.template) as fd:
+    #     template = fd.readlines()
+    #     template = ''.join(template)
+    #     template = template.decode("unicode_escape")
+    #     render(template, data)
+    #     with open("detail.html", 'wb') as fd:
+    #         fd.write(res)
 
 if __name__ == '__main__':
     main()
